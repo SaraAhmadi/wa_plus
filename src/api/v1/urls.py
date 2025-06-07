944678c7936a7888b906b9cc74f831d82941f2ea
s.taheri <taheri.sara1991@gmail.com>
2025-06-07 20:24:50 +0330

    verfy token added

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, RoleViewSet, PermissionViewSet, CustomTokenObtainPairView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -12,4 +12,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
